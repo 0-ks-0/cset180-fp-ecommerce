1c30292af546285cb6a8445ca9c2e5d46c3f3f6a
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-03 14:53:54 -0400

    Create customer test account for Jessica

--- src/server.py
+++ src/server.py
@@ -389,8 +389,9 @@ def get_account_type(user_id):
 # End of functions
 
 # Insert test values
-# Customer account
+# Customer accounts
 create_user_account("customer", "customer", "Customer", "Account", "c@c.c", "2e7d2c03a9507ae265ecf5b5356885a53393a2029d241394997265a1a25aefc6")
+create_user_account("customer", "lawrencetheflorence", "Jessica", "Lawrence", 'jessicalawrence@gmail.com', 'j')
 
 sql.commit()
 
