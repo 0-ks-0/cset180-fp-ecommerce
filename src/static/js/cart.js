bc8a7b172b808da10beaa5504199eb4c14cb401c
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-07 12:48:47 -0400

    Set up function addToCart with Ajax

--- src/static/js/cart.js
+++ src/static/js/cart.js
@@ -1,4 +1,30 @@
 function addToCart(id)
 {
+	$.ajax
+	({
+		url: '/products',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({'product_id': id}),
+		success: function(message)
+		{
+			const message_div = document.querySelector("#message")
 
+			if (!message_div) return
+
+			// Display message div if it is not showing
+			if (message_div.style.display == "none")
+			{
+				message_div.style.display = "block"
+			}
+
+			message_div.innerHTML = message
+
+			// Hide message div after 1s
+			setTimeout(() =>
+			{
+				message_div.style.display = "none"
+			}, 1000)
+		}
+	})
 }
