503121f90f88f9b6948b5d15b924d4be1a37c75f
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-12 14:48:49 -0400

    Create JS function removeItem for cart

--- src/static/js/cart.js
+++ src/static/js/cart.js
@@ -28,3 +28,43 @@ function addToCart(id)
 		}
 	})
 }
+
+/*
+*	Cart page
+*/
+function removeItem(element, cart_item_id)
+{
+	element.parentElement.remove()
+
+	fetch(`/cart`, {
+		headers:
+		{
+			"Content-Type" : "application/json"
+		},
+		method: "DELETE",
+		body: JSON.stringify({
+			"cart_item_id": cart_item_id
+		})
+	})
+	.then(function (response) // Callback function when response sent from server
+	{
+		// Check if status code between 200 and 300
+		if (response.ok)
+		{
+			return response.json() // Convert response from server to json
+
+			.then(response =>
+			{
+				top.location = response.url
+			})
+		}
+		else
+		{
+			throw Error(`Error: ${response.status || response.statusText}`)
+		}
+	})
+	.catch(error => // Catch errors from sending / receiving
+	{
+		console.log(error)
+	})
+}
