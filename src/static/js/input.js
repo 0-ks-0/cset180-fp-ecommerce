bf1a447c060f36c0cceb4d8cabe69171833d0842
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-14 14:19:48 -0400

    Add on to return for handleNegative

--- src/static/js/input.js
+++ src/static/js/input.js
@@ -3,5 +3,5 @@ function handleNegative(event, float = false)
 	if (float)
 		return (event.charCode != 8 && event.charCode == 0 || ((event.charCode >= 48 && event.charCode <= 57)  || event.charCode == 46))
 
-	return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 190
+	return (event.charCode != 8 && event.charCode == 0 || (event.charCode >= 48 && event.charCode <= 57))
 }
