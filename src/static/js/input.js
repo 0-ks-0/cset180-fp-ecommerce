30d29ded0e0a505556a37279c207fd2dc88cf363
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-14 14:19:59 -0400

    Add handleRating

--- src/static/js/input.js
+++ src/static/js/input.js
@@ -5,3 +5,13 @@ function handleNegative(event, float = false)
 
 	return (event.charCode != 8 && event.charCode == 0 || (event.charCode >= 48 && event.charCode <= 57))
 }
+
+/**
+ *
+ * @param {*} event
+ * @returns True if there is at most one digit in the input and the value is between 1 and 5. False otherwise
+ */
+function handleRating(event, element = null)
+{
+	return (event.charCode != 8 && event.charCode == 0 || ((event.charCode >= 49 && event.charCode <= 53))) && (element.value).length < 1
+}
