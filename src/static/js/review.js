2bd34e0f63220b88480914f91f56f43af4eb7dcd
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-15 23:59:10 -0400

    Create function submitReview

--- src/static/js/review.js
+++ src/static/js/review.js
@@ -29,3 +29,60 @@ function createReviewImage()
 
 	form.insertBefore(imageContainer, addButton)
 }
+
+function submitReview(event, product_id)
+{
+	event.preventDefault()
+
+	const data = {}
+
+	data.id = product_id
+
+	data.rating = document.getElementsByName("rating")[0].value
+
+	data.description = document.getElementsByName("description")[0].value
+
+	// Images
+	const image_inputs = document.getElementsByName("review_image")
+
+	const images = []
+
+	for (const input of image_inputs)
+	{
+		images.push(input.value)
+	}
+
+	data.images = images
+
+	console.log(data)
+
+	fetch(`/products//${product_id}`, {
+		headers:
+		{
+			"Content-Type" : "application/json"
+		},
+		method: "POST",
+		body: JSON.stringify(data)
+	})
+	.then(function (response) // Callback function when response sent from server
+	{
+		// Check if status code between 200 and 300
+		if (response.ok)
+		{
+			return response.json() // Convert response from server to json
+
+			.then(response =>
+			{
+				top.location = response.url
+			})
+		}
+		else
+		{
+			throw Error(`Error: ${response.status || response.statusText}`)
+		}
+	})
+	.catch(error => // Catch errors from sending / receiving
+	{
+		console.log(error)
+	})
+}
